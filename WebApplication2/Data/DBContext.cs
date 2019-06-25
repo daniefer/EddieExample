31a8380d0cbf83cb126ef535b2183b3123c81710
Daniel Ferguson <dan.ferguson@outlook.com>
2019-06-24 19:59:41 -0500

    init commit

---
+++ WebApplication2/Data/DBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TestClass> TestClasses { get; set; }
+    }
+
+    public class TestClass
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; }
+    }
+}
---
+++ WebApplication2/Migrations/20190625004032_FirstDataMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class FirstDataMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "TestClasses",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "TestClasses",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.Sql(@"update dbo.TestClasses
+                set 
+                    -- Assumes Value is some sort of Date string. For example: '2019-01-22 15:22:32.333'
+	                Date = CONVERT(date, SUBSTRING(Value, 0, 10)),
+	                Time = CONVERT(time, SUBSTRING(Value, 10, LEN(Value)))");
+
+            migrationBuilder.DropColumn(name: "Value", table: "TestClasses");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "TestClasses");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "TestClasses");
+
+            // Did not migration down the data migration
+        }
+    }
+}
